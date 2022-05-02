5a6f3be53b5ba25366be34291da3e40c4b421888
Wilson Tian <wilson.tian@perkinelmer.com>
2022-05-02 12:28:54 +0800

    add opencv.js

--- opencv-js-angular-example/src/app/app.component.ts
+++ opencv-js-angular-example/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import * as cv from '@techstark/opencv-js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'opencv-js-angular-example';
+  buildInfo = '';
+
+  constructor() {
+    setTimeout(() => {
+      (window as any).cv = cv;
+      this.buildInfo = cv.getBuildInformation();
+    }, 1_000);
+  }
 }
---
+++ opencv-js-angular-example/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  resolve: {
+    fallback: {
+      fs: false,
+      path: false,
+      crypto: false,
+    },
+  },
+};
