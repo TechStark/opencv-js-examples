6f2dd674f7f56cb6a396b9c98b305d82b9af3d25
Wilson Tian <wilson.tian@perkinelmer.com>
2022-07-23 16:13:26 +0800

    add opencv test page

---
+++ opencv-js-svelte-example/src/OpenCvTest.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import * as CV from "@techstark/opencv-js";
+
+  let buildInfo: string = "";
+
+  window.setTimeout(() => {
+    buildInfo = CV.getBuildInformation();
+  }, 1000);
+</script>
+
+<h1>opencv.js build information</h1>
+<textarea class="build-info">
+  {buildInfo}
+</textarea>
+
+<style>
+  .build-info {
+    width: 600px;
+    height: 400px;
+  }
+</style>
