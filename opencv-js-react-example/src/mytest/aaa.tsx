bd04fb690652d19e725515f7709f7f10e57b03e7
morninng <mr_morninng_star_dash@yahoo.co.jp>
2023-09-11 17:56:24 +0900

    realteime image analyze by opencv

--- opencv-js-react-example/src/mytest/aaa.tsx
+++ opencv-js-react-example/src/mytest/aaa.tsx
@@ -11,11 +11,13 @@ export function Aaa() {
 
   const videoRef = useRef<HTMLVideoElement>(null)
   const canvasRef = useRef<HTMLCanvasElement>(null)
+  const convertedCanvasRef = useRef<HTMLCanvasElement>(null)
   // const canvasContextRef = useRef<{context: CanvasRenderingContext2D | null}>({context: null}})
   // const ss = useRef<context: CanvasRenderingContext2D>({context: null}})
   const [context,setContext] = useState<CanvasRenderingContext2D>()
 
   useEffect(()=>{
+    console.log("useEffect");
 
     if(videoRef.current){
       videoRef.current.id       = 'video';
@@ -35,6 +37,8 @@ export function Aaa() {
     }).then(function(stream) {
       if(videoRef.current){
         videoRef.current.srcObject = stream;
+      }else{
+        console.log("videoRef.current is null");
       }
     });
 
@@ -43,27 +47,77 @@ export function Aaa() {
       canvasRef.current.width  = canvasSize.w;
       canvasRef.current.height = canvasSize.h;
       const aaa = canvasRef.current.getContext('2d');
+      if(aaa){
+        setInterval(()=>{
+          if(videoRef.current){
+            aaa.drawImage(videoRef.current, 0, 0, canvasSize.w, canvasSize.h);
+            requestAnimationFrame(_canvasUpdate);
+            canvasAnalyze(aaa);
+          }
+        }, 100);
+      }
+
+
+
       setContext(aaa!)
+    }else{
+      console.log("canvasRef.current is null");
     }
 
-    // setInterval(()=>{
-      _canvasUpdate();
+    if(convertedCanvasRef.current)  {
+      convertedCanvasRef.current.width  = canvasSize.w;
+      convertedCanvasRef.current.height = canvasSize.h;
+    }else{
+      console.log("convertedCanvasRef.current is null");
+    }
+
+
+
+    _canvasUpdate();
+
+    // setTimeout(()=>{
+    //   canvasAnalyze();
     // }, 1000);
-  })
+  }, [])
 
   const _canvasUpdate = () => {
     if(context && videoRef.current){
-      context.drawImage(videoRef.current, 0, 0, canvasSize.w, canvasSize.h);
-      requestAnimationFrame(_canvasUpdate);
+      // console.log("_canvasUpdate");
+
+    }else{
+      // console.log("context is null");
     }
   };
 
-  const canvasAnalyze = () => {
+  const canvasAnalyze = (context: CanvasRenderingContext2D) => {
+    console.log("canvasAnalyze");
     if(context){
+      console.log("sss");
       let imgData = context.getImageData(0, 0, canvasSize.w, canvasSize.h);
       let src = cv.matFromImageData(imgData);
 
+      const imgGray = new cv.Mat();
+      cv.cvtColor(src, imgGray, cv.COLOR_BGR2GRAY);
+      const edges = new cv.Mat();
+      cv.Canny(imgGray, edges, 50, 150);
 
+      if(convertedCanvasRef.current){
+        cv.imshow(convertedCanvasRef.current, edges);
+      }
+      src.delete();
+      imgGray.delete();
+      edges.delete();
+      // cv.cvtColor(src, imgGray, cv.COLOR_BGR2GRAY);
+      // cv.imshow(this.grayImgRef.current, imgGray);
+
+      // detect edges using Canny
+      // const edges = new cv.Mat();
+      // cv.Canny(imgGray, edges, 100, 100);
+      // cv.imshow(this.cannyEdgeRef.current, edges);
+
+      // // detect faces using Haar-cascade Detection
+      // const haarFaces = detectHaarFace(img);
+      // cv.imshow(this.haarFaceImgRef.current, haarFaces);
 
 
     }
@@ -76,8 +130,13 @@ export function Aaa() {
       video from
       <video ref={videoRef}></video>
       eee
+    <br />
+      <canvas  ref={canvasRef} />
+    <br />
+    jj
+      <canvas  ref={convertedCanvasRef} />
+      kk
 
-      <canvas className="canvas" ref={canvasRef} />
     </div>
   )
 }
