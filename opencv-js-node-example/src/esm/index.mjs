6e343bb70da73cae95e7f3ad68aaa492c28e12d6
Wilson Tian <wilson.tian@revvity.com>
2025-04-04 22:37:21 +0800

    fix jimp image

--- opencv-js-node-example/src/esm/index.mjs
+++ opencv-js-node-example/src/esm/index.mjs
@@ -1,4 +1,3 @@
-import path from "node:path";
 import { Jimp } from "jimp";
 import { getOpenCv } from "./opencv.mjs";
 
@@ -13,18 +12,22 @@ async function main() {
   cv.cvtColor(img, gray, cv.COLOR_RGBA2GRAY);
 
   // save the gray image to disk
+  // Note: Jimp does not support grayscale images directly
+  // convert gray image to RGBA format for Jimp
+  const rgbaGray = new cv.Mat();
+  cv.cvtColor(gray, rgbaGray, cv.COLOR_GRAY2RGBA);
   const grayJimp = new Jimp({
     width: gray.cols,
     height: gray.rows,
-    color: 0x00000000,
+    data: Buffer.from(rgbaGray.data),
   });
-  grayJimp.bitmap.data.set(gray.data);
   await grayJimp.write("output.png");
   console.log(`Saved gray image to output.png`);
 
   // release memory
   img.delete();
   gray.delete();
+  rgbaGray.delete();
   console.log("Memory released");
 }
 
