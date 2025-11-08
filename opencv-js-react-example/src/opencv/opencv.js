dff38f044dd06c9d70bfe97ad16523cf48e578a5
Wilson Tian <wilson.tian@revvity.com>
2025-11-08 13:54:56 +0800

    refactor: update OpenCV module initialization to handle promise correctly

--- opencv-js-react-example/src/opencv/opencv.js
+++ opencv-js-react-example/src/opencv/opencv.js
@@ -1,8 +1,15 @@
-import cvReadyPromise from "@techstark/opencv-js";
+import cvModule from "@techstark/opencv-js";
 
 export async function getOpenCv() {
-  const cv = await cvReadyPromise;
-  window.cv = cv;
+  let cv;
+  if (cvModule instanceof Promise) {
+    cv = await cvModule;
+  } else {
+    await new Promise((resolve) => {
+      cvModule.onRuntimeInitialized = () => resolve();
+    });
+    cv = cvModule;
+  }
   return cv;
 }
 
